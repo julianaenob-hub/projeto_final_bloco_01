210d412ae7ca7ab13bec2838abcf93f133683af8
Juliana Vitoria <julianaenob@gmail.com>
2026-06-30 11:36:35 -0300

    Implementar classes model e interface repository

---
+++ Livraria/src/model/Produto.java
@@ -0,0 +1,84 @@
+package model;
+
+public abstract class Produto {
+	
+	private int id;
+	private String nome;
+	private int tipo;
+	private float preco;
+	private int estoque;
+	
+	public Produto(int id, String nome, int tipo, float preco, int estoque) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.preco = preco;
+		this.estoque = estoque;
+		
+		
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public float getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	public int getEstoque() {
+	    return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+	    this.estoque = estoque;
+	}
+
+	
+	
+	public void visualizar() {
+
+	    System.out.println("\n\n***********************************************************");
+	    System.out.println("Dados do Produto:");
+	    System.out.println("***********************************************************");
+	    System.out.println("ID: " + this.id);
+	    System.out.println("Nome: " + this.nome);
+	    System.out.println("Tipo: " + this.tipo);
+	    System.out.println("Preço: R$ " + this.preco);
+	    System.out.println("Estoque: " + this.estoque);
+
+	}
+}
---
+++ Livraria/src/model/Revista.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Revista extends Produto {
+
+    private int edicao;
+
+    public Revista(int id, String nome, int tipo, float preco, int estoque, int edicao) {
+        super(id, nome, tipo, preco, estoque);
+        this.edicao = edicao;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+    @Override
+    public void visualizar() {
+        super.visualizar();
+        System.out.println("Edição: " + this.edicao);
+    }
+
+}
